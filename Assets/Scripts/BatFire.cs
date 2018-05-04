6632e596b9867f72568161c3de4e9002fcf2fabe
Josh NG <joshng@hotmail.co.uk>
2018-05-04 19:19:29 +0100

    Added new enemy
    Bat enemy Added stays still and attacks enemy with pellets

---
+++ Assets/Scripts/BatFire.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatFire : MonoBehaviour {
+
+	public GameObject bullet; // Gets Projectile.
+	Transform rightPos, leftPos, upPos, downPos; //Gets where the position of where the projectile will fire.
+	public float fireGap ; //Delay between firing.
+	private float timestamp; //A timestamp for the delay.
+	GameObject player;
+	float xSpeed;
+	float ySpeed;
+	Vector3 room = new Vector3 (0, 0, 0); //Which Room the Enemy Belongs to.
+	Vector3 spawn = new Vector3 (2, 0, 0); //Where the Enemy Initially Spawned.
+	//initialization
+	void Start () {
+		rightPos = transform.Find ("BulletRight"); //Finds Transforms.
+		leftPos = transform.Find ("BulletLeft");
+		upPos = transform.Find ("BulletUp");
+		downPos = transform.Find ("BulletDown");
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		if (inRoom () == true) {
+			FireLocation();
+		} 
+	}
+
+	bool inRoom(){
+		if (player.transform.position.x < room.x + 3.5 && player.transform.position.x > room.x - 3.5 && player.transform.position.y < room.y + 1.85 && player.transform.position.y > room.y - 1.85) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	void FireLocation() {
+			xSpeed = (player.transform.position.x - this.transform.position.x) / 2;
+			ySpeed = (player.transform.position.y - this.transform.position.y) / 2;
+			int direction = 0; 
+			if (xSpeed > ySpeed && xSpeed > 0) {
+				direction = 1;
+			} else if (ySpeed > xSpeed && ySpeed > 0) {
+				direction = 2;
+			} else if (xSpeed < ySpeed && xSpeed < 0) {
+				direction = 3;
+			} else if (ySpeed < xSpeed && ySpeed < 0) {
+				direction = 4;
+			}
+
+			//If Right Arrow is pressed Fire right and the delay has passed.
+			if (Time.time >= timestamp && direction == 1) {
+				Fire (rightPos);
+			}
+			//If Left Arrow is pressed Fire left and the delay has passed.
+			if (Time.time >= timestamp && direction == 2) {
+				Fire (leftPos);
+			}
+			//If up Arrow is pressed Fire up and the delay has passed.
+			if (Time.time >= timestamp && direction == 3) {
+				Fire (upPos);
+			}
+			//If down Arrow is pressed Fire down and the delay has passed.
+			if (Time.time >= timestamp && direction == 4) {
+				Fire (downPos);
+			}
+	}
+	//Fires the bullet
+	void Fire(Transform pos){
+
+		GameObject newBullet = Instantiate(bullet, pos.position, Quaternion.identity);
+		spitControl script = newBullet.GetComponent<spitControl> ();
+		script.speed = new Vector2 (xSpeed, ySpeed);
+		timestamp = Time.time + fireGap; //create delay.
+		Debug.Log("Time Stamp: " + timestamp);
+		Debug.Log ("Time: " + Time.time);
+	}
+}
--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour {
 	public GameObject room; //Room Prefab
@@ -14,10 +15,13 @@ public class StartGame : MonoBehaviour {
 	int room_Counter = 0; //Counter to keep count of rooms.
 	int num_Of_Rooms; //How many Rooms in the level.
 	Vector3[] roomPos; //Keeps the Position Of every Room.
+	int level = 0;
 
 	// Starts the Game
 	void Start () {
+		Instantiate (room, new Vector3 (0f, 0f, 0f), Quaternion.identity);
 		Instantiate(Player, new Vector3(0f, 0f, 0f), Quaternion.identity); //Spawns the Player.
+		level = 1;
 		Create_Level(); 
 	}
 	//Creates the level.
@@ -26,39 +30,41 @@ public class StartGame : MonoBehaviour {
 		roomPos = new Vector3[num_Of_Rooms]; //Makes Array of room positions of how many rooms being created.
 		Vector3 pos = new Vector3(0f, 0f, 1f); //sets first rooms position.
 		room_Counter = 0; //Sets counter to 0.
-		Create_Room (pos, 2); //Creates room with initial Position.
+		Create_Room (pos, 2, 0); //Creates room with initial Position.
 		Create_Ladder (); //Spawns ladder in Random Room.
 		CreateEnemies (); //Creates Enemies in rooms.
 	}
 	//Creates Rooms.
-	void Create_Room(Vector3 pos, int min){
+	void Create_Room(Vector3 pos, int min, int preDir){
 		//If number of rooms remaining is greater than 4 must create 2 more rooms.
 		if (num_Of_Rooms > 4) {
 			min = 2;
 		}
-
 		int num_Of_Doors = Random.Range (min, 4 % num_Of_Rooms); //Gets a random number of rooms to create.
 		num_Of_Rooms = num_Of_Rooms - num_Of_Doors; //Decrease the amount of rooms needed to create.
 		Instantiate (room, pos, Quaternion.identity); //Create a room.
 		roomPos [room_Counter] = pos; //Sets the position of the Created Room.
 		room_Counter++; //Increase the counter of the room.
-		int[] currentdoors = new int[num_Of_Doors]; //Creates a array of the doors in the room.
-		int counter = 0; //Sets a ccounter for the doors.
+		int[] currentdoors = new int[num_Of_Doors+1]; //Creates a array of the doors in the room.
+		currentdoors[0] = preDir;
+		int counter = 1; //Sets a ccounter for the doors.
 		int dir; //Create variable for the direction where the door will be. 
 		bool dirTaken; //boolean to check if the direction has been taken.
 		//For loop to create door.
 		for (int i = 0; i < num_Of_Doors; i++) {
+			Debug.Log ("This is stil the preDir " + currentdoors [0]);
 			do {
 				dirTaken = false; //Sets taken direction to false.
 				dir = Random.Range (1, 4); //Picks a random Direction.
 				//Checks if direction has been taken.
 				for (int j = 0; j < currentdoors.Length; j++) {
 					if (dir == currentdoors[j]) {
+						Debug.Log("This Direction has been taken" + dir);
 						dirTaken = true;
 					}
 				}
 			} while(dirTaken != false); //If directions taken do again and pick another.
-
+			Debug.Log("This the direction we have taken" + dir);
 			currentdoors[counter] = dir; //Sets Door Direction.
 			counter++; 
 			Create_Door (dir, pos); //Creates door.
@@ -71,22 +77,22 @@ public class StartGame : MonoBehaviour {
 		if (direction == 1) {
 			Instantiate (rightDoor, new Vector3 (pos.x + 3.44f, pos.y, pos.z - 1), Quaternion.identity); //Create a door on the Right.
 			Instantiate (leftDoor, new Vector3 (pos.x + 4.55f, pos.y, pos.z - 1), Quaternion.identity); //Create door on the left of the next room.
-			Create_Room (new Vector3(pos.x + 8f, pos.y, pos.z), 0); //Create a room to the Right.
+			Create_Room (new Vector3(pos.x + 8f, pos.y, pos.z), 0, 2); //Create a room to the Right.
 		}
 		else if (direction == 2) {
 			Instantiate (leftDoor, new Vector3 (pos.x - 3.44f, pos.y, pos.z - 1), Quaternion.identity);//Create a door on the Left.
 			Instantiate (rightDoor, new Vector3 (pos.x - 4.55f, pos.y, pos.z - 1), Quaternion.identity); //Create door on the right of the next room.
-			Create_Room (new Vector3(pos.x - 8f, pos.y, pos.z), 0);//Create a room to the Left.
+			Create_Room (new Vector3(pos.x - 8f, pos.y, pos.z), 0, 1);//Create a room to the Left.
 		}
 		else if (direction == 3) {
 			Instantiate (upDoor, new Vector3 (pos.x, pos.y + 1.96f, pos.z - 1), Quaternion.Euler(0,0,90));//Create a door on the top.
 			Instantiate (downDoor, new Vector3 (pos.x, pos.y + 2.73f, pos.z - 1), Quaternion.Euler(0,0,90)); //Create door on the bottom of the next room.
-			Create_Room (new Vector3(pos.x , pos.y + 4.55f, pos.z), 0);//Create a room upwards.
+			Create_Room (new Vector3(pos.x , pos.y + 4.55f, pos.z), 0, 4);//Create a room upwards.
 		}
 		else if (direction == 4) {
 			Instantiate (downDoor, new Vector3 (pos.x, pos.y - 1.96f, pos.z - 1f), Quaternion.Euler(0,0,-90));//Create a door on the bottom.
 			Instantiate (upDoor, new Vector3 (pos.x, pos.y - 2.73f, pos.z - 1), Quaternion.Euler(0,0,90)); //Create door on the top of the next room.
-			Create_Room (new Vector3(pos.x, pos.y - 4.55f, pos.z), 0);//Create a room at the bottom.
+			Create_Room (new Vector3(pos.x, pos.y - 4.55f, pos.z), 0, 3);//Create a room at the bottom.
 		}
 	}
 	//Creates new level.
@@ -99,6 +105,10 @@ public class StartGame : MonoBehaviour {
 		destroy_Objects ("Ladder");
 		Debug.Log("Destroying Ladders");
 		destroy_Objects ("Enemy");
+		level++;
+		if (level == 3) {
+			SceneManager.LoadScene ("Winning");
+		}
 		Create_Level(); //Creates new level.
 		GameObject player = GameObject.FindGameObjectWithTag ("Player"); //Sets player to starting position.
 		player.transform.position = new Vector3 (0f, 0f, 0f);
@@ -122,8 +132,6 @@ public class StartGame : MonoBehaviour {
 		while (ladder_Space != true) {
 
 			space = Random.Range (1, roomPos.Length - 1);
-			Debug.Log (roomPos.Length);
-			Debug.Log ("space is " + space);
 
 			if (roomPos [space] != new Vector3(0, 0, 1)) {
 
@@ -142,9 +150,7 @@ public class StartGame : MonoBehaviour {
 			int amountOfEnemies = Random.Range (1, 4);
 			for (int i = 1; i < amountOfEnemies; i++) {
 				float randomX = Random.Range (roomPos [num].x - 2, roomPos [num].x + 2);
-				Debug.Log ("x" + randomX);
 				float randomy = Random.Range (roomPos [num].y - 2, roomPos [num].y + 2);
-				Debug.Log ("Y" + randomy);
 				GameObject newEnemy = Instantiate(Enemy, new Vector3(randomX, randomy, 0), Quaternion.identity);
 				EnemyMovement script = newEnemy.GetComponent<EnemyMovement>();
 				script.SetRoom (new Vector3(roomPos [num].x, roomPos [num].y, roomPos [num].z));
